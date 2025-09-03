6676e6d7e4266b95c55f1415479b6469c6cf0231
thanh26122002 <thanhdinh@aidia.vn>
2025-09-03 09:42:57 +0700

    create this new branch to test some git command

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,29 +1,26 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from "@eslint/js";
+import globals from "globals";
+import react from "eslint-plugin-react";
 
-export default defineConfig([
-  globalIgnores(['dist']),
+export default [
+  js.configs.recommended,
   {
-    files: ['**/*.{js,jsx}'],
-    extends: [
-      js.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
-    ],
+    files: ["**/*.{js,jsx}"],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
-      parserOptions: {
-        ecmaVersion: 'latest',
-        ecmaFeatures: { jsx: true },
-        sourceType: 'module',
-      },
+      sourceType: "module",
+    },
+    plugins: {
+      react,
     },
     rules: {
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      // your rules here
+    },
+    settings: {
+      react: {
+        version: "detect", // ✅ auto-detect React version
+      },
     },
   },
-])
+];
